15258bb7a36c55ab8b88f2ad2eba98f5ac0f633f
grobbie <rob@barbarians.io>
2018-10-04 10:35:46 +0200

    adapt Ignite control scripts for POSIX shell compliance
    Signed-off-by: grobbie <rob@barbarians.io>

--- bin/ignite.sh
+++ bin/ignite.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
@@ -111,7 +111,7 @@ fi
 #
 # Uncomment to set preference for IPv4 stack.
 #
-# JVM_OPTS="${JVM_OPTS} -Djava.net.preferIPv4Stack=true"
+JVM_OPTS="${JVM_OPTS} -Djava.net.preferIPv4Stack=true"
 
 #
 # Assertions are disabled by default since version 3.5.
